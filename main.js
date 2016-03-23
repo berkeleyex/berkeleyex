1a85abf30e610b01d3d4bfce5b11a7b883302aea
yugan <yugan@amazon.com>
2016-03-23 01:04:44 -0700

    hook up form to google forms

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    document.getElementById('signUpForm').onsubmit = function () {
+        return validateForm();
+    };
+});
+
+function validateForm () {
+    // TODO
+    return true;
+}
