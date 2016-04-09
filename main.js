4426afe81512af3bde033dc7a3cd4ffe8895785d
yugan <yugan@amazon.com>
2016-04-09 11:27:14 -0700

    render sign-up div

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ $(document).ready(function () {
     document.getElementById('signUpForm').onsubmit = function () {
         return validateForm();
     };
+
+    document.getElementById('dummy').onload = function () {
+        onSignup();
+    };
 });
 
 function validateForm () {
     // TODO
     return true;
+}
+
+function onSignup () {
+    $('#thankYou').css('display', 'block');
 }
